b571173c70ae06f4fc4bafec16624220cad2402f
vayulink <vayulink24@gmail.com>
2014-12-29 20:53:42 +0530

    test site commit

---
+++ js/loader.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$("#content").load("contact.html");
+
+});
+
+$('.menu li a').click(function(e){
+	var page = $(this).attr('href');
+	$('.menu li').removeClass('active');
+   var $parent = $(this).parent();
+	   if (!$parent.hasClass('active')) {
+			$parent.addClass('active');
+      }
+   e.preventDefault();
+	$("#content").load(page);
+});
